3997e7aee8b2d245c84f50c59dbf50e25b935a27
rahmounikaoutar5-code <rahmounikaoutar5@gmail.com>
2025-12-29 16:33:00 +0100

    Add storage layer skeleton

---
+++ src/storage/SubjectStorage.java
@@ -0,0 +1,15 @@
+package storage;
+
+import model.Subject;
+import java.util.List;
+
+public class SubjectStorage {
+
+    public void save(List<Subject> subjects) {
+        // XML persistence will be implemented later
+    }
+
+    public List<Subject> load() {
+        return List.of();
+    }
+}
